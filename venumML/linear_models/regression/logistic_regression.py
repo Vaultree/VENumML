7dcccac2e5489755b01aa8e1dbe952bda5f0c8b1
marco-vaultree <marco.lam@vaultree.com>
2025-01-06 17:11:43 +0000

    Bug fix

--- venumML/linear_models/regression/logistic_regression.py
+++ venumML/linear_models/regression/logistic_regression.py
@@ -3,6 +3,21 @@ import numpy as np
 from venumML.venum_tools import *
 
 def sigmoid_approximation(ctx, x):
+    """@private
+
+    Parameters
+    ----------
+    ctx : type
+        Description of ctx.
+    x : type
+        Description of x.
+
+    Returns
+    -------
+    result : type
+        Description of result.
+    """
+
     # Define the core logic as an inner function to facilitate vectorization
     def core_sigmoid(ctx, x):
         tc1, tc2, tc3 = ctx.encrypt(0.5), ctx.encrypt(0.25), ctx.encrypt(0.02)
@@ -16,19 +31,58 @@ def sigmoid_approximation(ctx, x):
 
 
 class EncryptedLogisticRegression:
+    """
+    Logistic Regression model that supports encrypted computations.
+
+    Attributes
+    ----------
+    _context : object
+        Encryption context to perform encrypted operations.
+    _coef_ : numpy.ndarray or None
+        Model coefficients (weights).
+    _intercept_ : float
+        Intercept (bias) term.
+    _encrypted_coef_ : numpy.ndarray
+        Encrypted model coefficients (weights).
+    _encrypted_intercept_ : object
+        Encrypted intercept (bias) term.
+    """
     
     def __init__(self, ctx):
-        self.context = ctx
-        self.coef_ = None  # Will be initialized based on number of features
-        self.intercept_ = 0  # Scalar for the bias term
+        """
+        Initialise the EncryptedLogisticRegression model.
+
+        Parameters
+        ----------
+        ctx : object
+            Encryption context to handle encrypted operations.
+        """
+
+        self._context = ctx
+        self._coef_ = None  # Will be initialized based on number of features
+        self._intercept_ = 0  # Scalar for the bias term
     
     def fit(self, X, y, num_iterations=1000, learning_rate=0.1):
+        """
+        Fit the model to the provided data using gradient descent.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Feature matrix (n_samples, n_features).
+        y : numpy.ndarray
+            Target vector (n_samples,).
+        num_iterations : int, optional
+            Number of iterations for gradient descent (default is 1000).
+        learning_rate : float, optional
+            Learning rate for gradient descent (default is 0.1).
+        """
         n_samples, n_features = X.shape
-        self.coef_ = np.zeros(n_features)  # Initialize coefficients as a vector
-        self.intercept_ = 0  # Initialize intercept (bias)
+        self._coef_ = np.zeros(n_features)  # Initialize coefficients as a vector
+        self._intercept_ = 0  # Initialize intercept (bias)
 
         for _ in range(num_iterations):
-            linear_model = np.dot(X, self.coef_) + self.intercept_  # Linear combination
+            linear_model = np.dot(X, self._coef_) + self._intercept_  # Linear combination
             y_predicted = 1 / (1 + np.exp(-linear_model))  # Sigmoid function
 
             # Gradient calculations
@@ -36,14 +90,29 @@ class EncryptedLogisticRegression:
             db = (1 / n_samples) * np.sum(y_predicted - y)  # Derivative w.r.t. bias
 
             # Parameter updates
-            self.coef_ -= learning_rate * dw
-            self.intercept_ -= learning_rate * db
+            self._coef_ -= learning_rate * dw
+            self._intercept_ -= learning_rate * db
 
     def encrypted_fit(self, ctx, x, y, lr=0.3, gamma=0.9, epochs=3):
         """
-        Fits the logistic regression model using encrypted data, Nesterov optimization, 
-        and a sigmoid approximation.
+        Fit the model using encrypted data using encrypted data, Nesterov optimization, and a sigmoid approximation.
+
+        Parameters
+        ----------
+        ctx : object
+            Encryption context for handling encrypted operations.
+        x : numpy.ndarray
+            Encrypted feature matrix (n_samples, n_features).
+        y : numpy.ndarray
+            Encrypted target vector (n_samples,).
+        lr : float, optional
+            Learning rate for optimisation (default is 0.3).
+        gamma : float, optional
+            Momentum term for Nesterov optimisation (default is 0.9).
+        epochs : int, optional
+            Number of epochs for training (default is 3).
         """
+        
         encrypted_gamma = ctx.encrypt(gamma)
         encrypted_lr = ctx.encrypt(lr)
 
@@ -77,7 +146,6 @@ class EncryptedLogisticRegression:
             grad_w = np.mean(x * error, axis=0, keepdims=True).T
             grad_b = np.mean(error)
 
-
             # Update velocities
             # print(velocity_w,grad_w,encrypted_lr,gamma)
             velocity_w = velocity_w * encrypted_gamma + grad_w * encrypted_lr
@@ -88,15 +156,40 @@ class EncryptedLogisticRegression:
             b = b - velocity_b
 
         # Store encrypted parameters
-        self.encrypted_coef_ = np.atleast_1d(w.squeeze())
-        self.encrypted_intercept_ = b
+        self._encrypted_coef_ = np.atleast_1d(w.squeeze())
+        self._encrypted_intercept_ = b
 
     def encrypt_coefficients(self, ctx):
-        self.encrypted_intercept_ = ctx.encrypt(self.intercept_)
-        self.encrypted_coef_ = [ctx.encrypt(v) for v in self.coef_]
+        """
+        Encrypt the model's coefficients and intercept.
+
+        Parameters
+        ----------
+        ctx : object
+            Encryption context to perform encrypted operations.
+        """
+
+        self._encrypted_intercept_ = ctx.encrypt(self._intercept_)
+        self._encrypted_coef_ = [ctx.encrypt(v) for v in self._coef_]
 
     def predict(self, encrypted_X, ctx):
+        """
+        Predict outcomes using encrypted data.
+
+        Parameters
+        ----------
+        encrypted_X : numpy.ndarray
+            Encrypted feature matrix (n_samples, n_features).
+        ctx : object
+            Encryption context for handling encrypted operations.
+
+        Returns
+        -------
+        object
+            Encrypted predictions.
+        """
+
         # Compute the linear model with encrypted coefficients and intercept
-        linear_model = np.dot(encrypted_X, self.encrypted_coef_) + self.encrypted_intercept_
+        linear_model = np.dot(encrypted_X, self._encrypted_coef_) + self._encrypted_intercept_
 
         return linear_model
